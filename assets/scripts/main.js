7bdde42d71c5879e936d1f6fd948f37d02c4dd07
yuw006 <63177486+yuw006@users.noreply.github.com>
2021-02-06 01:48:18 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,14 +2,18 @@
 var airHorn = document.getElementById('radio-air-horn');
 var carHorn = document.getElementById('radio-car-horn');
 var partyHorn = document.getElementById('radio-party-horn');
+var horn = document.getElementById('horn-sound');
 var soundImage = document.getElementById("sound-image");
 
 airHorn.addEventListener('click', function (){
-                                    soundImage.src = "./assets/media/images/air-horn.svg";}, false);
+                                    soundImage.src = "./assets/media/images/air-horn.svg";
+                                    horn.src="./assets/media/audio/air-horn.mp3";}, false);
 carHorn.addEventListener('click', function (){
-                                    soundImage.src = "./assets/media/images/car.svg";}, false);
+                                    soundImage.src = "./assets/media/images/car.svg";
+                                    horn.src="./assets/media/audio/car-horn.mp3";}, false);
 partyHorn.addEventListener('click', function (){
-                                      soundImage.src = "./assets/media/images/party-horn.svg";}, false);
+                                      soundImage.src = "./assets/media/images/party-horn.svg";
+                                      horn.src="./assets/media/audio/party-horn.mp3";}, false);
 
 
 
