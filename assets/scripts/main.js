5862e70fa9685bb0c4322241ef1e6018ddded3a6
yuw006 <63177486+yuw006@users.noreply.github.com>
2021-02-05 16:53:09 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,20 @@
 // main.js
+var airHorn = document.getElementById("radio-air-horn");
+var carHorn = document.getElementById("radio-car-horn");
+var partyHorn = document.getElementById("radio-party-horn");
+var soundImage = document.getElementById("soundImage");
 
+airHorn.addEventListner("click",changeToAirHorn);
+carHorn.addEventListner("click",changeToCarHorn);
+partyHorn.addEventListner("click",changeToPartyHorn);
+
+function changeToAirHorn(){
+  soundImage.src = "./assets/media/images/air-horn.svg"; 
+}
+function changeToCarHorn(){
+  soundImage.src = "./assets/media/images/car-horn.svg"; 
+}
+function changeToPartyHorn(){
+  soundImage.src = "./assets/media/images/party-horn.svg"; 
+}
 // TODO
