d147411eb63bb1b8506fdb69f68608d37a442f7f
yuw006 <63177486+yuw006@users.noreply.github.com>
2021-02-06 00:50:18 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,22 +1,16 @@
 // main.js
-var airHorn = document.getElementById("radio-air-horn");
-var carHorn = document.getElementById("radio-car-horn");
-var partyHorn = document.getElementById("radio-party-horn");
-var soundImage = document.getElementById("soundImage");
+var airHorn = document.getElementById('radio-air-horn');
+var carHorn = document.getElementById('radio-car-horn');
+var partyHorn = document.getElementById('radio-party-horn');
+var soundImage = document.getElementById("sound-image");
 
-airHorn.addEventListener("onclick",changeToAirHorn);
-carHorn.addEventListener("onclick",changeToCarHorn);
-partyHorn.addEventListener("onclick",changeToPartyHorn);
+airHorn.addEventListener('click', function (){
+                                    soundImage.src = "./assets/media/images/air-horn.svg";}, false);
+carHorn.addEventListener('click', function (){
+                                    soundImage.src = "./assets/media/images/car.svg";}, false);
+partyHorn.addEventListener("onclick", function (){
+                                      soundImage.src = "./assets/media/images/party-horn.svg";}, false);
 
-function changeToAirHorn(){
-  soundImage.src = "./assets/media/images/air-horn.svg"; 
-}
-function changeToCarHorn(){
-  soundImage.src = "./assets/media/images/car.svg"; 
-}
-function changeToPartyHorn(){
-  soundImage.src = "./assets/media/images/party-horn.svg"; 
-}
 
 
 // TODO
