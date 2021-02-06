b423d457254fd7f74823b0c78981e8364dc5d4bd
yuw006 <63177486+yuw006@users.noreply.github.com>
2021-02-06 01:20:56 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,3 @@
 // main.js
-document.getElementById('honk-btn').addEventListener( 'click', function() { document.getElementById('horn-sound').play(); });
+document.getElementById('honk-btn').addEventListener( 'click', function() { document.getElementById('horn-sound').play(); }, false);
 // TODO
