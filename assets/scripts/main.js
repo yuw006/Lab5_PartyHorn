17e43d49fc2717556e7ae5423b2425e956ad1039
yuw006 <63177486+yuw006@users.noreply.github.com>
2021-02-06 01:15:46 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,23 +5,29 @@ var horn = document.getElementById("horn-sound");
 var volumeImage = document.getElementById("volume-image");
 
 var volumeText = document.getElementById("volume_text");
-volumeText.addEventListener("onChange", setVolume(volumeText.value));
+volumeText.addEventListener('change', setVolume(volumeText.value), false);
 var volumeSlider = document.getElementById("volume_slider");
-volumeSlider.addEventListener("onChange", setVolume(volumeSlider.value));
+volumeSlider.addEventListener('change', setVolume(volumeSlider.value), false);
 
 //sets the volume based on the text field
-function setVolume(volume){
-  horn.volume = (setVolumeByText()/100.00).toFixed(2);
-  changeIcon();
+function setVolume(v){
+  horn.v = (setVolumeByText()/100.00).toFixed(2);
+  changeIcon(v);
 }
 
 //sets the icon based on the volume
-function changeIcon(){
-  if( 0.67 <= horn.volume <= 1.0){
+function changeIcon(v){
+  if( 67 <= v ){
     volumeImage.src = "./assets/media/icons/volume-level-3.svg";
   }
-  else if( 0.34 <= horn.volume <= 0.66 ){
+  else if( 33 <= v ){
     volumeImage.src = "./assets/media/icons/volume-level-2.svg";
+  }
+  else if( 1 <= v ){
+    volumeImage.src = "./assets/media/icons/volume-level-1.svg";
+  }
+  else {
+    volumeImage.src = "./assets/media/icons/volume-level-0.svg";
   }
 }
                                    
